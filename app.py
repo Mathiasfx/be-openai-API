20e8358086459d1425c326e56e4f722ec1f4097c
andrescuello7 <andrescuellotrabajo@gmail.com>
2024-06-02 02:00:33 -0300

    update cors activate

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from routes.openai_routes import app
-    
+from flask_cors import CORS
+
+# Cors routes
+CORS(app)
+
 # Server running from Flask
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=8000, host='0.0.0.0')
